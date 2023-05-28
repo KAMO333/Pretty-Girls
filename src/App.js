b6dce4fd2719f277c3eade7b2cc29a60e30b1ce8
Moroka <mmopanemoroka@gmail.com>
2023-05-28 16:11:19 +0100

    added initial code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Products from "./components/products/Products";
+import Slider from "./components/slider/Slider";
+import Testimonials from "./components/testimonials/Testimonials";
+import Virtual from "./components/virtual/Virtual";
+import Footer from "./components/footer/Footer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <span>Subscribe Zainkeepscode</span>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+import {
+  InboxIcon,
+  PhoneIcon,
+  LocationMarkerIcon,
+  LoginIcon,
+  UsersIcon,
+  LinkIcon,
+} from "@heroicons/react/outline";
+import css from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={css.cFooterWrapper}>
+      <hr />
+      <div className={css.cFooter}>
+        <div className={css.logo}>
+          <img src={Logo} alt="" />
+          <span>Pretty Girls</span>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.detail}>
+            <span>Contact Us</span>
+            <span className={css.pngLine}>
+              <LocationMarkerIcon className={css.icon} />
+              <span>321 South Avenue Road, Johannesburg </span>
+            </span>
+            <span className={css.pngLine}>
+              {""}
+              <PhoneIcon className={css.icon} />
+              <a href="tell:352-386-4415">065 254 25187</a>
+            </span>
+            <span className={css.pngLine}>
+              <InboxIcon className={css.icon} />
+              <a href="mailto:support@amazon.com">Support@Pretty-Girls.com</a>
+            </span>
+          </div>
+        </div>
+        <div className={css.block}>
+          <div className={css.detail}>
+            <span>Account</span>
+            <span className={css.pngLine}>
+              <LoginIcon className={css.icon} />
+              Sign In
+            </span>
+          </div>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.detail}>
+            <span>Company</span>
+            <span className={css.pngLine}>
+              <UsersIcon className={css.icon} />
+              <a href="/about">
+                <p>About Us</p>
+              </a>
+            </span>
+          </div>
+        </div>
+
+        <div className={css.block}>
+          <div className={css.detail}>
+            <span>Resources</span>
+            <span className={css.pngLine}>
+              <LinkIcon className={css.icon} />
+              <p>Safety Privacy & Terms</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className={css.copyRight}>
+        <span>Copyright ©2023 by Pretty Girls, Inc.</span>
+        <span>All rights reserved.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
